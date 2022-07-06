443f0d8c2fea40333e998b4aaad63b9f69768d5b
Ricardo <ricardovelero@gmail.com>
2022-07-06 13:18:17 +0200

    crear Clien, Add

--- src/app/components/client-add/client-add.component.ts
+++ src/app/components/client-add/client-add.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Client } from "src/app/models/client.model";
+import { ClientService } from "src/app/services/client.service";
 
 @Component({
-  selector: 'app-client-add',
-  templateUrl: './client-add.component.html',
-  styleUrls: ['./client-add.component.css']
+  selector: "app-client-add",
+  templateUrl: "./client-add.component.html",
+  styleUrls: ["./client-add.component.css"],
 })
-export class ClientAddComponent implements OnInit {
+export class AddClientComponent implements OnInit {
+  client: Client = {
+    name: "",
+    address: "",
+    telephone: "",
+    email: "",
+    isactive: false,
+  };
+  submitted = false;
 
-  constructor() { }
+  constructor(private clientService: ClientService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  saveClient(): void {
+    const data = {
+      name: this.client.name,
+      address: this.client.address,
+      telephone: this.client.telephone,
+      email: this.client.email,
+      isactive: (this.client.isactive = true),
+    };
+    this.clientService.create(data).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.submitted = true;
+      },
+      error: (e) => console.error(e),
+    });
   }
-
+  newClient(): void {
+    this.submitted = false;
+    this.client = {
+      name: "",
+      address: "",
+      telephone: "",
+      email: "",
+      isactive: true,
+    };
+  }
 }
