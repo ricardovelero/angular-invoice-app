6dfbb2c8b71ad973b06581f246f256242d2ccd8a
Ricardo <ricardovelero@gmail.com>
2022-07-06 12:53:39 +0200

    clients list

---
+++ src/app/components/clients-list/clients-list.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from "@angular/core";
+import { Client } from "src/app/models/client.model";
+import { ClientService } from "src/app/services/client.service";
+
+@Component({
+  selector: "app-clients-list",
+  templateUrl: "./clients-list.component.html",
+  styleUrls: ["./clients-list.component.css"],
+})
+export class ClientsListComponent implements OnInit {
+  clients?: Client[];
+  currentClient: Client = {};
+  currentIndex = -1;
+  name: string = "";
+
+  constructor(private clientService: ClientService) {}
+
+  ngOnInit(): void {
+    this.retrieveClients();
+  }
+  retrieveClients(): void {
+    this.clientService.getAll().subscribe({
+      next: (data) => {
+        this.clients = data;
+        console.log(data);
+      },
+      error: (e) => console.error(e),
+    });
+  }
+  refreshList(): void {
+    this.retrieveClients();
+    this.currentClient = {};
+    this.currentIndex = -1;
+  }
+  setActiveClient(client: Client, index: number): void {
+    this.currentClient = client;
+    this.currentIndex = index;
+  }
+  setInactiveClient(client: Client, index: number): void {
+    this.currentClient = client;
+    this.currentIndex = index;
+  }
+  removeAllClients(): void {
+    this.clientService.deleteAll().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.refreshList();
+      },
+      error: (e) => console.error(e),
+    });
+  }
+  searchName(): void {
+    this.currentClient = {};
+    this.currentIndex = -1;
+    this.clientService.findByName(this.name).subscribe({
+      next: (data) => {
+        this.clients = data;
+        console.log(data);
+      },
+      error: (e) => console.error(e),
+    });
+  }
+}
