e1752cec169a0987064ee0f99fadab718c6b22ef
Ricardo <ricardovelero@gmail.com>
2022-07-07 13:53:59 +0200

    complete client model

--- src/app/models/client.model.ts
+++ src/app/models/client.model.ts
@@ -1,8 +1,14 @@
 export class Client {
   id?: any;
   name?: string;
-  address?: string;
-  telephone?: string;
+  lastname?: string;
   email?: string;
+  telephone?: string;
+  mobile?: string;
+  businessemail?: string;
+  businesstelephone?: string;
+  nifnumber?: string;
+  creditlimit?: number;
+  address?: string;
   isactive?: boolean;
 }
