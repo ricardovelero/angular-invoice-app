9b8ad4ea4a2ecd9d065644c54512a3389248771e
Ricardo <ricardovelero@gmail.com>
2022-07-18 18:29:10 +0200

    Adding components and structuring the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'fz-app';
+  title = "fz-app";
+  todayDate: Date = new Date();
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
+  menuToggle: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  opencloseMenu() {
+    if (!this.menuToggle) {
+      this.menuToggle = !this.menuToggle;
+    } else {
+      this.menuToggle = !this.menuToggle;
+    }
+    return this.menuToggle;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
