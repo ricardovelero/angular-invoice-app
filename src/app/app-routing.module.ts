2a38642a806205ae6efa82066272ea6e4cd64e96
Ricardo <ricardovelero@gmail.com>
2022-07-18 20:25:51 +0200

    Fix menus and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,12 @@ import { ClientAddComponent } from "./components/client-add/client-add.component
 import { ClientDetailsComponent } from "./components/client-details/client-details.component";
 import { ClientsListComponent } from "./components/clients-list/clients-list.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { InvoicesListComponent } from "./components/invoices-list/invoices-list.component";
 
 const routes: Routes = [
   { path: "", component: DashboardComponent },
+  { path: "invoices", component: InvoicesListComponent },
+  { path: "invoice-add", component: InvoicesListComponent },
   { path: "clients", component: ClientsListComponent },
   { path: "clients/:id", component: ClientDetailsComponent },
   { path: "client-add", component: ClientAddComponent },
