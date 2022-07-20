9102b2a39e75dadbd26d11cb870eb9ef275fad1b
Ricardo <ricardovelero@gmail.com>
2022-07-20 16:26:13 +0200

    Retrieve client

--- src/app/components/invoice-add/invoice-add.component.ts
+++ src/app/components/invoice-add/invoice-add.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Client } from "../../models/client.model";
+import { ClientService } from "../../services/client.service";
 
 @Component({
-  selector: 'app-invoice-add',
-  templateUrl: './invoice-add.component.html',
-  styleUrls: ['./invoice-add.component.css']
+  selector: "app-invoice-add",
+  templateUrl: "./invoice-add.component.html",
+  styleUrls: ["./invoice-add.component.css"],
 })
 export class InvoiceAddComponent implements OnInit {
+  clients?: Client[];
 
-  constructor() { }
+  constructor(private clientService: ClientService) {}
 
   ngOnInit(): void {
+    this.retrieveClients();
   }
 
+  retrieveClients(): void {
+    this.clientService.getAllClients().subscribe({
+      next: (data) => {
+        this.clients = data;
+        console.log(data);
+      },
+      error: (e) => console.error(e),
+    });
+  }
 }
