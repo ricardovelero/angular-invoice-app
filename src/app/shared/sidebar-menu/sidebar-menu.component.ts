432700645971412c05f82344ce228a77ba696a0b
Ricardo <ricardovelero@gmail.com>
2022-07-20 16:10:48 +0200

    Add mobile menu functionality

--- src/app/shared/sidebar-menu/sidebar-menu.component.ts
+++ src/app/shared/sidebar-menu/sidebar-menu.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-sidebar-menu',
-  templateUrl: './sidebar-menu.component.html',
-  styleUrls: ['./sidebar-menu.component.css']
+  selector: "app-sidebar-menu",
+  templateUrl: "./sidebar-menu.component.html",
+  styleUrls: ["./sidebar-menu.component.css"],
 })
 export class SidebarMenuComponent implements OnInit {
+  menuToggle: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  opencloseMenu() {
+    if (!this.menuToggle) {
+      this.menuToggle = !this.menuToggle;
+    } else {
+      this.menuToggle = !this.menuToggle;
+    }
+    return this.menuToggle;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
