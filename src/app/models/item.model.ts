bf7bd97e742a1b8f9cea62c80be7a030ccc96029
Ricardo <ricardovelero@gmail.com>
2022-07-21 14:53:43 +0200

    Item model and service

---
+++ src/app/models/item.model.ts
@@ -0,0 +1,10 @@
+export class Client {
+  id?: number;
+  item_name?: string;
+  item_description?: string;
+  unit_cost?: number;
+  quantity?: number;
+  tax1?: string;
+  tax2?: string;
+  actual_price?: number;
+}
