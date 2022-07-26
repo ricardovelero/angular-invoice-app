74bb89859d93e9d6e5c8ecb1055b208bfb9d5d0e
Ricardo <ricardovelero@gmail.com>
2022-07-26 19:32:52 +0200

    Change name

--- src/app/components/invoice-add/invoice-add.component.ts
+++ src/app/components/invoice-add/invoice-add.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Client } from "../../models/client.model";
+import { Item } from "../../models/item.model";
 import { ClientService } from "../../services/client.service";
+import { ItemService } from "../../services/item.service";
 
 import { PrimeNGConfig } from "primeng/api";
 
@@ -8,17 +10,25 @@ import { PrimeNGConfig } from "primeng/api";
   selector: "app-invoice-add",
   templateUrl: "./invoice-add.component.html",
   styleUrls: ["./invoice-add.component.css"],
-  providers: [ClientService],
+  providers: [ClientService, ItemService],
 })
 export class InvoiceAddComponent implements OnInit {
   modalToggle: boolean = false;
-  clients: Client[] = [];
 
-  selectedClientAdvanced?: any[];
+  clients: Client[] = [];
+  items: Item[] = [];
+
+  selectedClient?: Client[];
   filteredClients: any[] | any;
+
+  selectedItem?: Item[];
+  filteredItems: any[] | any;
+
+  qty: number = 1;
 
   constructor(
     private clientService: ClientService,
+    private itemService: ItemService,
     private primengConfig: PrimeNGConfig
   ) {}
 
@@ -27,12 +37,22 @@ export class InvoiceAddComponent implements OnInit {
     this.clientService.getAllClients().subscribe((clients) => {
       this.clients = clients;
     });
+    this.itemService.getAllItems().subscribe((items) => {
+      this.items = items;
+    });
   }
 
   filterClient(event: any) {
     let query = event.query;
     this.clientService.findClientByName(query).subscribe((clients) => {
       this.filteredClients = clients;
+    });
+  }
+
+  filterItem(event: any) {
+    let query = event.query;
+    this.itemService.findItemByName(query).subscribe((items) => {
+      this.filteredItems = items;
     });
   }
 
