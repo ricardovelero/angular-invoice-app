4c3ca03ec2a655ff3f0422c71443c8fd0eba2065
Ricardo <ricardovelero@gmail.com>
2022-08-03 17:54:30 +0200

    Add auth0 funcionality New colors for sidebar New logo New Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { AuthService } from "@auth0/auth0-angular";
 
 @Component({
   selector: "app-root",
@@ -6,6 +7,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = "fz-app";
-  todayDate: Date = new Date();
+  constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {}
 }
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, Inject, OnInit } from "@angular/core";
+import { AuthService } from "@auth0/auth0-angular";
+import { DOCUMENT } from "@angular/common";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent implements OnInit {
+  constructor(
+    @Inject(DOCUMENT) public document: Document,
+    public auth: AuthService
+  ) {}
+
+  ngOnInit(): void {}
+}
