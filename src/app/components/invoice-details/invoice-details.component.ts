ceb7ff31653e61ff7c878c10ca35d8767da70681
Ricardo <ricardovelero@gmail.com>
2022-08-16 20:40:21 +0200

    Invoice detail component

--- src/app/components/invoice-details/invoice-details.component.ts
+++ src/app/components/invoice-details/invoice-details.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+import { Invoice } from "../../models/invoice.model";
+import { InvoiceService } from "../../services/invoice.service";
+import { PrimeNGConfig } from "primeng/api";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-invoice-details',
-  templateUrl: './invoice-details.component.html',
-  styleUrls: ['./invoice-details.component.css']
+  selector: "app-invoice-details",
+  templateUrl: "./invoice-details.component.html",
+  styleUrls: ["./invoice-details.component.css"],
+  providers: [InvoiceService],
 })
 export class InvoiceDetailsComponent implements OnInit {
+  @Input() currentInvoice: Invoice | any;
 
-  constructor() { }
+  constructor(
+    private invoiceService: InvoiceService,
+    private primengConfig: PrimeNGConfig,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.primengConfig.ripple = true;
+    this.getInvoice(this.route.snapshot.params["id"]);
   }
-
+  getInvoice(id: string): void {
+    this.invoiceService.getInvoice(id).subscribe({
+      next: (data) => {
+        this.currentInvoice = data;
+      },
+      error: (e) => console.error(e),
+    });
+  }
 }
