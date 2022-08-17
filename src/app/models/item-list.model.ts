c3592fd3db48ac0ce65f50915c6b3ac53c57f64b
Ricardo <ricardovelero@gmail.com>
2022-08-17 10:55:35 +0200

    Update model fields

--- src/app/models/item-list.model.ts
+++ src/app/models/item-list.model.ts
@@ -1,7 +1,6 @@
 export class ItemList {
-  item_name?: string;
-  unit_cost?: number;
+  name?: string;
+  cost?: number;
   quantity?: number;
   tax1?: string;
-  total?: number;
 }
--- src/app/models/item.model.ts
+++ src/app/models/item.model.ts
@@ -1,10 +1,8 @@
 export class Item {
   id?: string;
-  item_name?: string;
-  item_description?: string;
-  unit_cost?: number;
-  quantity?: number;
+  name?: string;
+  description?: string;
+  cost?: number;
   tax1?: string;
   tax2?: string;
-  actual_price?: number;
 }
