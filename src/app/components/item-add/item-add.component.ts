6c1f7162e0bbebb507388f423b2f572dd0219531
Ricardo <ricardovelero@gmail.com>
2022-08-22 13:27:37 +0200

    Add item

--- src/app/components/item-add/item-add.component.ts
+++ src/app/components/item-add/item-add.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { ItemService } from "src/app/services/item.service";
 
 @Component({
   selector: "app-item-add",
@@ -6,7 +9,39 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./item-add.component.css"],
 })
 export class ItemAddComponent implements OnInit {
-  constructor() {}
+  @Output() displayItemModal = new EventEmitter<boolean>();
+  @Output() theItem = new EventEmitter<object>();
+
+  submitted: boolean = false;
+
+  itemForm = new FormGroup({
+    id: new FormControl(""),
+    name: new FormControl("", [Validators.required, Validators.minLength(2)]),
+    description: new FormControl(""),
+    cost: new FormControl("", [Validators.required]),
+    tax1: new FormControl("", [Validators.required]),
+    tax2: new FormControl("", [Validators.required]),
+  });
+
+  constructor(private itemService: ItemService, private router: Router) {}
 
   ngOnInit(): void {}
+
+  addItem(): void {
+    const data = this.itemForm.value;
+    this.itemService.createItem(data).subscribe({
+      next: (res) => {
+        this.submitted = true;
+        this.theItem.emit(res);
+      },
+      error: (e) => console.error(e),
+    });
+  }
+
+  toItemList(): void {
+    this.router.navigate(["items"]);
+  }
+  parentModal(value: boolean) {
+    this.displayItemModal.emit(value);
+  }
 }
