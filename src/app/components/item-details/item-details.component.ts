2ba825da1652ed1d89ba717ef112f34c7da71618
Ricardo <ricardovelero@gmail.com>
2022-08-26 18:26:30 +0200

    Item details view

--- src/app/components/item-details/item-details.component.ts
+++ src/app/components/item-details/item-details.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Item } from "src/app/models/item.model";
 
 @Component({
-  selector: 'app-item-details',
-  templateUrl: './item-details.component.html',
-  styleUrls: ['./item-details.component.css']
+  selector: "app-item-details",
+  templateUrl: "./item-details.component.html",
+  styleUrls: ["./item-details.component.css"],
 })
 export class ItemDetailsComponent implements OnInit {
+  @Input() inItem: Item = {};
+  @Output() displayModal = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
