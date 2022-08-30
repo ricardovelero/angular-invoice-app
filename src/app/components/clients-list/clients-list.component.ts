08df4510fbeedd830921b061df2fadd91aace37c
Ricardo <ricardovelero@gmail.com>
2022-08-30 13:18:50 +0200

    Table view and edit

--- src/app/components/clients-list/clients-list.component.ts
+++ src/app/components/clients-list/clients-list.component.ts
@@ -3,11 +3,43 @@ import { Router } from "@angular/router";
 import { Client } from "../../models/client.model";
 import { ClientService } from "../../services/client.service";
 import { Table } from "primeng/table";
+import {
+  ConfirmationService,
+  MessageService,
+  PrimeNGConfig,
+} from "primeng/api";
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
 
 @Component({
   selector: "app-clients-list",
   templateUrl: "./clients-list.component.html",
   styleUrls: ["./clients-list.component.css"],
+  animations: [
+    trigger("rowExpansionTrigger", [
+      state(
+        "void",
+        style({
+          transform: "translateX(-10%)",
+          opacity: 0,
+        })
+      ),
+      state(
+        "active",
+        style({
+          transform: "translateX(0)",
+          opacity: 1,
+        })
+      ),
+      transition("* <=> *", animate("400ms cubic-bezier(0.86, 0, 0.07, 1)")),
+    ]),
+  ],
+  providers: [ClientService],
 })
 export class ClientsListComponent implements OnInit {
   @ViewChild("dt") dt: Table | undefined;
@@ -17,17 +49,34 @@ export class ClientsListComponent implements OnInit {
   @Input() viewMode = false;
 
   modalToggle: boolean = false;
-  clients?: Client[];
+  clients: Client[] = [];
+  client: Client | any;
   currentClient: Client = {};
   currentIndex = -1;
   name: string = "";
 
+  selectedClients: Client[] = [];
+
   showSpinner: boolean = true;
   isEmpty: boolean = false;
+  isCard: boolean = false;
 
-  constructor(private clientService: ClientService, private router: Router) {}
+  submitted: boolean = false;
+  clientDialog: boolean = false;
+  clientEditDialog: boolean = false;
+
+  isSubmitDissabled: boolean = true;
+
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private primengConfig: PrimeNGConfig,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
+    this.primengConfig.ripple = true;
     this.retrieveClients();
     this.showSpinner = false;
   }
@@ -81,5 +130,118 @@ export class ClientsListComponent implements OnInit {
       },
       error: (e) => console.error(e),
     });
+  }
+  deleteSelectedClients() {}
+
+  editClient(client: Client) {
+    this.client = { ...client };
+    this.clientEditDialog = true;
+  }
+
+  eraseClient(client: Client | any) {
+    this.confirmationService.confirm({
+      message: "¿Está seguro de que quiere borrar " + client.fullName + "?",
+      header: "Por favor, confirmar...",
+      acceptLabel: "Sí",
+      rejectLabel: "No",
+      icon: "pi pi-exclamation-triangle",
+      rejectButtonStyleClass: "p-button-text",
+      accept: () => {
+        this.clientService.deleteClient(client.id).subscribe({
+          next: (res) => {
+            console.log(res);
+            this.messageService.add({
+              severity: "success",
+              summary: "Operación exitosa",
+              detail: "Cliente borrado",
+              life: 3000,
+            });
+            this.clients = this.clients.filter((val) => val.id !== client.id);
+          },
+          error: (e) =>
+            this.messageService.add({
+              severity: "error",
+              summary: "Error en la operación",
+              detail: "El ítem no ha podido ser borrado",
+              life: 5000,
+            }),
+        });
+        this.client = {};
+      },
+    });
+  }
+  saveClient() {
+    this.submitted = true;
+    if (this.client.fullName.trim()) {
+      if (this.client.id) {
+        this.clients[this.findIndexById(this.client.id)] = this.client;
+        this.clientService.updateClient(this.client.id, this.client).subscribe({
+          next: (res) => {
+            this.messageService.add({
+              severity: "success",
+              summary: "Exitoso",
+              detail: "Item actualizado",
+              life: 3000,
+            });
+          },
+          error: (e) =>
+            this.messageService.add({
+              severity: "error",
+              summary: "Error",
+              detail: e,
+              life: 5000,
+            }),
+        });
+      } else {
+        this.clientService.createClient(this.client).subscribe({
+          next: (res) => {
+            this.messageService.add({
+              severity: "success",
+              summary: "Exitoso",
+              detail: "Item creado",
+              life: 3000,
+            });
+            this.clients.push(res);
+          },
+          error: (e) =>
+            this.messageService.add({
+              severity: "error",
+              summary: "Error",
+              detail: e,
+              life: 5000,
+            }),
+        });
+      }
+      this.clients = [...this.clients];
+      this.clientDialog = false;
+      this.client = {};
+    }
+  }
+  findIndexById(id: number) {
+    let index = -1;
+    for (let i = 0; i < this.clients.length; i++) {
+      if (this.clients[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+
+    return index;
+  }
+  showModalDialog() {
+    this.clientDialog = true;
+  }
+  clientFromChild(client: any) {
+    client = { ...client, Invoices: [] };
+    this.clients.push(client);
+  }
+  openNew() {
+    this.client = {};
+    this.submitted = false;
+    this.clientDialog = true;
+  }
+  hideDialog() {
+    this.clientEditDialog = false;
+    this.submitted = false;
   }
 }
