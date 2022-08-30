14c02fc8483f6294aed9afe8f9a61d4229c50640
Ricardo <ricardovelero@gmail.com>
2022-08-30 20:54:10 +0200

    New user Profile

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,13 @@
+export interface User {
+  id?: number;
+  fullName?: string;
+  nif?: string;
+  street?: string;
+  zipcode?: string;
+  city?: string;
+  province?: string;
+  country?: string;
+  company?: string;
+  email?: string;
+  phone?: string;
+}
