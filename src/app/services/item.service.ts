8c822ce6a93dc98f373f83adbaa82b44bb4319da
Ricardo <ricardovelero@gmail.com>
2022-08-31 21:32:37 +0200

    Better organization and back to remote

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -2,11 +2,10 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Item } from "../models/item.model";
-//import { address as env } from "../../environments/environment";
+import { address as env } from "../../environments/environment";
 
-//const baseUrl = `${env.remote}/api/items`;
+const baseUrl = `${env.remote}/api/items`;
 // const baseUrl = `${env.local}/api/items`;
-const baseUrl = "https://facturazen-backend-9tcee.ondigitalocean.app/api/items";
 
 @Injectable({
   providedIn: "root",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 const domain = "fzdev.eu.auth0.com";
 const clientId = "MkO5fxCBtJmQ4TvLCO7wXfzKhW6XeuJp";
 const audience = "https://api.facturazen.es";
-// const serverUrl = "https://localhost:8080";
-const serverUrl = "https://facturazen-backend-9tcee.ondigitalocean.app";
+const serverUrl = "https://localhost:8080";
+//const serverUrl = "https://facturazen-backend-9tcee.ondigitalocean.app";
 
 export const environment = {
   production: false,
