f2c4db3bb5e298ca1df21d4a1b30bc2821f23828
Ricardo <ricardovelero@gmail.com>
2022-09-02 17:03:07 +0200

    New user profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { AuthService } from "@auth0/auth0-angular";
+import { UserService } from "../../services/user.service";
+import { MessageService } from "primeng/api";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { passwordMatchingValidator } from "../../shared/input/confirm-password.validator";
+import { passwordStrengthValidator } from "../../shared/input/password-strength.validator";
 
 @Component({
   selector: "app-profile",
@@ -9,9 +14,62 @@ import { AuthService } from "@auth0/auth0-angular";
 export class ProfileComponent implements OnInit {
   userProfile: string | any;
 
-  constructor(public auth: AuthService) {}
+  isEditting: boolean = false;
+
+  passwordForm = new FormGroup(
+    {
+      password: new FormControl("", [
+        Validators.required,
+        Validators.minLength(8),
+        passwordStrengthValidator,
+      ]),
+      confirmPassword: new FormControl("", [Validators.required]),
+    },
+    { validators: [passwordMatchingValidator] }
+  );
+
+  newPassword: string | any;
+  repeatPassword: string | any;
+  pwDialog: boolean = false;
+  showPassword: boolean = false;
+
+  constructor(
+    public auth: AuthService,
+    private userService: UserService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.auth.user$.subscribe((profile) => (this.userProfile = profile));
+  }
+
+  changePassword() {
+    const data = { newPassword: this.passwordForm.controls.password.value };
+    this.userService.changeUserPassword(data).subscribe({
+      next: (res) => {
+        this.pwDialog = false;
+        this.messageService.add({
+          severity: "success",
+          summary: "Exitoso",
+          detail: "Contraseña cambiada",
+          life: 3000,
+        });
+      },
+      error: (e) => {
+        this.messageService.add({
+          severity: "error",
+          summary: "Error",
+          detail: e.error.message,
+          life: 5000,
+        });
+      },
+    });
+  }
+  showErrors() {
+    const { invalid, dirty, touched } = this.passwordForm;
+    return invalid && (dirty || touched);
+  }
+  showHidePassword() {
+    this.showPassword = !this.showPassword;
   }
 }
