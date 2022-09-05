e2bd6f887a27680587b4d5a54147581a6be295b1
Ricardo <ricardovelero@gmail.com>
2022-09-05 10:05:39 +0200

    Mozilla, Safari add an aditional / to url Had to use full address in variable

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -4,8 +4,8 @@ import { Observable } from "rxjs";
 import { Item } from "../models/item.model";
 import { address as env } from "../../environments/environment";
 
-const baseUrl = `${env.remote}/api/items`;
-// const baseUrl = `${env.local}/api/items`;
+const baseUrl = "https://facturazen-backend-9tcee.ondigitalocean.app/api/items";
+// const baseUrl = "http://localhost:8080/api/items",;
 
 @Injectable({
   providedIn: "root",
