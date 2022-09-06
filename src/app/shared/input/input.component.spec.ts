e1bf1925052214117cde6b34e6fb70503895ad90
Ricardo <ricardovelero@gmail.com>
2022-09-06 10:25:26 +0200

    Add shadow when enabled

--- src/app/shared/input/input.component.spec.ts
+++ src/app/shared/input/input.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { InputComponent } from './input.component';
+import InputComponent from "./input.component";
 
-describe('InputComponent', () => {
+describe("InputComponent", () => {
   let component: InputComponent;
   let fixture: ComponentFixture<InputComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InputComponent ]
-    })
-    .compileComponents();
+      declarations: [InputComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(InputComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
