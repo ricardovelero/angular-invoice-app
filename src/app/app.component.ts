aaaf9656af1131f4a0cdb6cc69601cd5136eb663
Ricardo <ricardovelero@gmail.com>
2022-09-20 14:30:31 +0200

    Comment out LocalSotorage logic for Problems with Safari

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,29 +17,29 @@ export class AppComponent {
   constructor(public auth: AuthService, private http: HttpClient) {}
 
   ngOnInit(): void {
-    if (sessionStorage.getItem("profileComplete") === "true")
-      this.profileComplete = true;
-    this.loginsCount = sessionStorage.getItem("logins_count");
-    if (!sessionStorage.getItem("UserId") && !this.loginsCount) {
-      this.auth.user$
-        .pipe(
-          concatMap((user) =>
-            // Use HttpClient to make the call
-            this.http.get(
-              encodeURI(`https://fzdev.eu.auth0.com/api/v2/users/${user?.sub}`)
-            )
-          ),
-          tap((meta) => {
-            this.metadata = meta;
-            console.log(this.metadata);
-            sessionStorage.setItem(
-              "UserId",
-              this.metadata.app_metadata.apiUserId
-            );
-            sessionStorage.setItem("logins_count", this.metadata.logins_count);
-          })
-        )
-        .subscribe();
-    }
+    // if (sessionStorage.getItem("profileComplete") === "true")
+    //   this.profileComplete = true;
+    // this.loginsCount = sessionStorage.getItem("logins_count");
+    // if (!sessionStorage.getItem("UserId") && !this.loginsCount) {
+    //   this.auth.user$
+    //     .pipe(
+    //       concatMap((user) =>
+    //         // Use HttpClient to make the call
+    //         this.http.get(
+    //           encodeURI(`https://fzdev.eu.auth0.com/api/v2/users/${user?.sub}`)
+    //         )
+    //       ),
+    //       tap((meta) => {
+    //         this.metadata = meta;
+    //         console.log(this.metadata);
+    //         sessionStorage.setItem(
+    //           "UserId",
+    //           this.metadata.app_metadata.apiUserId
+    //         );
+    //         sessionStorage.setItem("logins_count", this.metadata.logins_count);
+    //       })
+    //     )
+    //     .subscribe();
+    // }
   }
 }
