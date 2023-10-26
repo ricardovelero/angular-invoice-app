54ef397e83c6521dbb8558cc9d3e9312d7b5bf0c
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-10-26 20:25:09 +0200

    New uri for auth0

--- src/app/components/firstlogin/firstlogin.component.ts
+++ src/app/components/firstlogin/firstlogin.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { CdkStepper } from "@angular/cdk/stepper";
-import { AbstractControl, FormBuilder } from "@angular/forms";
-import { Country } from "src/app/models/country.model";
-import { CountryService } from "src/app/services/country.service";
-import { UserService } from "../../services/user.service";
-import { MessageService } from "primeng/api";
-import { Router } from "@angular/router";
-import { AuthService } from "@auth0/auth0-angular";
-import { HttpClient } from "@angular/common/http";
-import { concatMap, tap } from "rxjs";
+import { Component, OnInit, ViewChild } from "@angular/core"
+import { CdkStepper } from "@angular/cdk/stepper"
+import { AbstractControl, FormBuilder } from "@angular/forms"
+import { Country } from "src/app/models/country.model"
+import { CountryService } from "src/app/services/country.service"
+import { UserService } from "../../services/user.service"
+import { MessageService } from "primeng/api"
+import { Router } from "@angular/router"
+import { AuthService } from "@auth0/auth0-angular"
+import { HttpClient } from "@angular/common/http"
+import { concatMap, tap } from "rxjs"
 
 @Component({
   selector: "app-firstlogin",
@@ -16,7 +16,7 @@ import { concatMap, tap } from "rxjs";
   styleUrls: ["./firstlogin.component.css"],
 })
 export class FirstloginComponent implements OnInit {
-  @ViewChild("cdkStepper") cdkStepper: CdkStepper | undefined;
+  @ViewChild("cdkStepper") cdkStepper: CdkStepper | undefined
 
   frmStepper = this.fb.group({
     steps: this.fb.array([
@@ -33,9 +33,9 @@ export class FirstloginComponent implements OnInit {
         }),
       }),
     ]),
-  });
+  })
 
-  countries: Country[] | any;
+  countries: Country[] | any
 
   constructor(
     private fb: FormBuilder,
@@ -50,22 +50,22 @@ export class FirstloginComponent implements OnInit {
       .at(0)
       .controls.address.controls.country?.setValue("España", {
         onlySelf: true,
-      });
+      })
   }
 
   ngOnInit(): void {
     this.countryService.getCountries().then((data) => {
-      this.countries = data;
-    });
+      this.countries = data
+    })
   }
 
   // formArray getter
   get formArray(): AbstractControl {
-    return this.frmStepper.controls.steps;
+    return this.frmStepper.controls.steps
   }
 
   submit(): void {
-    this.frmStepper.disable;
+    this.frmStepper.disable
     const {
       fullName,
       nif,
@@ -73,7 +73,7 @@ export class FirstloginComponent implements OnInit {
       address: {
         value: { street, city, zip, region, country },
       },
-    } = this.frmStepper.controls.steps.at(0).controls;
+    } = this.frmStepper.controls.steps.at(0).controls
     const data = {
       fullName: fullName.value,
       nif: nif.value,
@@ -84,7 +84,7 @@ export class FirstloginComponent implements OnInit {
       province: region,
       country: country,
       profileComplete: true,
-    };
+    }
     const auth0Data = {
       name: fullName,
       phone_number: phone.value,
@@ -93,44 +93,46 @@ export class FirstloginComponent implements OnInit {
         address: `${street}, ${city}, ${region}, ${zip}, ${country}`,
         profileComplete: true,
       },
-    };
+    }
     this.auth.user$
       .pipe(
         concatMap((user) =>
           // Use HttpClient to make the call
           this.http.patch(
-            encodeURI(`https://fzdev.eu.auth0.com/api/v2/users/${user?.sub}`),
+            encodeURI(
+              `https://dev-gyc750vd.us.auth0.com/api/v2/users/${user?.sub}`
+            ),
             auth0Data
           )
         ),
         tap((res) => {
-          console.log(res);
+          console.log(res)
         })
       )
-      .subscribe();
+      .subscribe()
     this.userService.updateUser(data).subscribe({
       next: (res) => {
-        sessionStorage.setItem("profileComplete", "true");
+        sessionStorage.setItem("profileComplete", "true")
         this.messageService.add({
           severity: "success",
           summary: "Hecho",
           detail: "Datos actualizados",
           life: 3000,
-        });
+        })
         // this.router.navigate(["invoice-add"]);
         setTimeout(() => {
-          window.location.assign("invoice-add");
-        }, 3000);
+          window.location.assign("invoice-add")
+        }, 3000)
       },
       error: (e) => {
-        console.error(e);
+        console.error(e)
         this.messageService.add({
           severity: "error",
           summary: "Error",
           detail: e.error.message,
           life: 5000,
-        });
+        })
       },
-    });
+    })
   }
 }
